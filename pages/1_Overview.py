f79b1eeae6eaef034fa48ee772379b8a249cdc68
Sohel Modi <59948950+Sohel-coder@users.noreply.github.com>
2025-04-30 01:19:25 +0530

    Update 1_Overview.py

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -141,10 +141,6 @@ st.markdown('''
 </h4>
 ''', unsafe_allow_html=True)
 
-# "Begin Analysis" button to navigate to Military Strength section
-if st.button("Begin Analysis", key="begin_analysis"):
-    st.session_state['page'] = 'Military Strength'
-    st.experimental_rerun()
     
 # Statistics section
 st.markdown('<h2 style="text-align:center; color:#1a237e; margin:2rem 0;">Global Military Overview</h2>', unsafe_allow_html=True)
@@ -202,3 +198,7 @@ st.markdown('''
 </div>
 ''', unsafe_allow_html=True)
 st.markdown('</div>', unsafe_allow_html=True)
+
+# "Begin Analysis" button navigates via URL query parameter
+if st.button("Begin Analysis"):
+    st.experimental_set_query_params(page="Military Strength")
